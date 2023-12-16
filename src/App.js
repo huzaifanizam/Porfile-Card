abf8b2ed75d919776fc4977ed4406472e790b731
huzaifanizam <nizamhuzaifa@gmail.com>
2023-12-16 19:33:15 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Porfile from './pages/Porfile';
+
+function App() {
+  return (
+    <div className="App">
+      <Porfile />
+    </div>
+  );
+}
+
+export default App;
